1347a12b9285ec6f9374ee8782ae31324886fb7a
JonathanPlasse <13716151+JonathanPlasse@users.noreply.github.com>
2020-08-31 21:10:07 +0200

    Change to matplotlib

--- tbs.py
+++ tbs.py
@@ -1,5 +1,5 @@
 import numpy as np
-import pyqtgraph as pg
+import matplotlib.pyplot as plt
 
 def R(t):
     return np.array([[np.cos(t),np.sin(t)],[-np.sin(t),np.cos(t)]])
@@ -11,22 +11,28 @@ def plot_tb(r0, rs0, v):
     r_t = np.array([r(theta, r0, t_i, p, rs0, v) for t_i in t])
     plt.plot(r_t[:,0],r_t[:,1])
 
-theta = 80*np.pi/180
 t_final = 10
-p = 1
 n = 1000
 r0 = np.array([0,0])
 
-t = [i/(n-1)*t_final for i in range(n)]
+for j, p in enumerate(np.linspace(0.04, 2, 50)):
+    for deg in range(10, 100, 10):
+        theta = deg*np.pi/180
+        t = [i/(n-1)*t_final for i in range(n)]
 
-plt = pg.PlotWindow()
-plt.setAspectLocked()
+        rs0 = np.array([t_final,0])
+        v = np.array([0,0])
+        plot_tb(r0, rs0, v)
 
-rs0 = np.array([10,0])
-v = np.array([0,0])
-plot_tb(r0, rs0, v)
 
-rs0 = np.array([0,0])
-v = np.array([1,0])
-plot_tb(r0, rs0, v)
+        # rs0 = np.array([0,0])
+        # v = np.array([1,0])
+        # plot_tb(r0, rs0, v)
 
+    plt.axis('equal')
+    plt.xlim(-5, 25)
+    plt.ylim(-15, 15)
+    plt.title('Toilet Bowl Échelon p={:.2f}'.format(p))
+
+    plt.savefig('tb_ramp_p'+str(j).zfill(2))
+    plt.cla()
